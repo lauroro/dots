18e07bb24a4065fec8b30612bc716cfbdd52a195
lauroro <lauroro@protonmail.com>
2022-11-18 16:05:03 +0100

    fix wofi networkmenu

--- configs/wofi/network/network.sh
+++ configs/wofi/network/network.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 
-CFG="$HOME/.config/hypr/network/config"
+CFG="$HOME/.config/wofi/network/config"
 STYLE="$HOME/.config/wofi/style.css"
 
 
 connect() {
-  ssid=$(nmcli device wifi list | sed -n '1!p' | cut -b 9- | wofi -c "$CFG" -s "$STYLE" --show dmenu --prompt "Scanning networks..." | cut -d' ' -f3)
+  ssid=$(nmcli device wifi list | sed -n '1!p' | cut -b 28- | wofi -c "$CFG" -s "$STYLE" --show dmenu --prompt "Scanning networks..." | cut -d' ' -f3)
   if [[ ! -z "$ssid" ]];then
     pass=$(echo -e 'Type password for connection "$ssid"' | wofi -c "$CFG" -s "$STYLE" --show dmenu --prompt "Type password here.")
     nmcli device wifi connect "$ssid" password "$pass" || nmcli device wifi connect "$ssid"
